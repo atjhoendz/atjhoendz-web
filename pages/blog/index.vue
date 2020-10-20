9d009770e63a1faff0c4812022711866db35de03
atjhoendz <achunarmando@gmail.com>
2020-10-20 18:29:57 +0700

    update list blog page

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,22 +2,52 @@
   <v-container>
     <h1 class="text-center">List Blog</h1>
     <v-list>
-      <v-list-item v-for="(blog, i) in blogs" :key="i" :to="blog.path">{{
-        blog.title
-      }}</v-list-item>
+      <v-list-item
+        v-for="(blog, i) in blogs"
+        :key="i"
+        :to="{ name: 'blog-slug', params: { slug: blog.slug } }"
+        nuxt
+      >
+        <v-list-item-content>
+          <v-list-item-title
+            class="headline"
+            v-text="blog.title"
+          ></v-list-item-title>
+          <v-list-item-subtitle
+            v-text="blog.description"
+          ></v-list-item-subtitle>
+        </v-list-item-content>
+        <v-list-item-action>
+          <v-list-item-action-text v-text="formatDate(blog.createdAt)">
+          </v-list-item-action-text>
+        </v-list-item-action>
+      </v-list-item>
     </v-list>
   </v-container>
 </template>
 
 <script>
 export default {
-  async asyncData({ $content }) {
-    const blogs = await $content('blog').fetch()
+  async asyncData({ $content, params }) {
+    const blogs = await $content('blog', params.slug)
+      .only(['title', 'description', 'img', 'slug', 'author', 'createdAt'])
+      .sortBy('createdAt', 'desc')
+      .fetch()
 
     return {
       blogs,
     }
   },
+  methods: {
+    formatDate(date) {
+      const options = { year: 'numeric', month: 'long', day: 'numeric' }
+      return new Date(date).toLocaleDateString('id', options)
+    },
+  },
+  head: () => ({
+    title: 'Blog',
+  }),
+  loading: true,
 }
 </script>
 
