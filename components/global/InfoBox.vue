009aac1ab85d34d8383d1a538a902d541ab0d792
atjhoendz <achunarmando@gmail.com>
2020-10-20 18:29:35 +0700

    add author and infobox component

---
+++ components/global/InfoBox.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="blue white--text pa-4 mb-4">
+    <p><slot name="info-box">default</slot></p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
