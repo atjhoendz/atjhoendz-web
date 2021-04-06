c09fe5c5243b45dc3a222ea442ecf4b8f89be4ad
atjhoendz <achunarmando@gmail.com>
2021-04-06 17:32:48 +0700

    chore(tailwind): add colour pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: '#1F2235',
+        lightBlue: '#313552',
+        lightYellow: '#FFC276',
+        black: '#000000',
+        white: '#FFFFFF',
+      },
+    },
   },
   variants: {
     extend: {},
