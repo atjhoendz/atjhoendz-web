ca19000c30e2db52a06b4cab3318771e387a0fdd
atjhoendz <achunarmando@gmail.com>
2021-04-08 14:24:24 +0700

    chore(tailwind-config): add color

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,27 @@ module.exports = {
         white: '#FFFFFF',
         cyan: '#00FFFF',
         ebonyClay: '#232737',
+        vuejs: '#41B883',
+        bootstrap: '#5a3e79',
+        laravel: '#F05340',
+        docker: '#0db7ed',
+        ubuntu: '#DD4814',
+        github: '#211F1F',
+        git: '#f34f29',
+        vscode: '#0078d7',
       },
       fontFamily: {
         nunito: ['nunito', 'sans-serif'],
         playFair: ['Playfair Display', 'serif'],
+      },
+      keyframes: {
+        'arrow-right': {
+          '0%, 100%': { transform: 'translateX(-25%)' },
+          '50%': { transform: 'translateX(0)' },
+        },
+      },
+      animation: {
+        'arrow-right': 'arrow-right 1s ease-in-out infinite',
       },
     },
   },
